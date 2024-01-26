7b109fa3648c70795b05ab80bbabc5320bc0cf50
Gregory <95241798+Gregory-Coelho@users.noreply.github.com>
2024-01-26 14:55:50 +0100

    initiate react / serv /

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { ArgentBankWebsite } from "../src/ArgentBankWebsite";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <ArgentBankWebsite />
+    </div>
+  );
+}
+
+export default App;
