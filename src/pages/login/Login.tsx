7fbc18d5a83706142e171f9a58d2a978afbc86f3
Gregory <95241798+Gregory-Coelho@users.noreply.github.com>
2024-03-24 09:34:41 +0100

    wip login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { AppDispatch } from "../../store/store.tsx";
 import React from "react";
 import { loginUser } from "../../store/authStore.tsx";
+import { getUserProfile } from "../../../server/services/userService.js";
 
 export const Login = () => {
   const dispatch: AppDispatch = useDispatch();
@@ -11,6 +12,20 @@ export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(""); // Ajout de l'état pour le message d'erreur
+
+  console.log(errorMessage);
+  console.log(rememberMe);
+
+  const fetchUserProfile = async () => {
+    try {
+      // Dispatch de l'action getUserProfile pour récupérer le profil de l'utilisateur
+      await dispatch(getUserProfile());
+    } catch (error) {
+      // Si une erreur se produit, capturez-la et affichez un message d'erreur approprié
+      setErrorMessage("Wrong password or username");
+    }
+  };
 
   // Fonction de soumission du formulaire de connexion
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -18,7 +33,6 @@ export const Login = () => {
     // Dispatch de l'action loginUser avec les informations d'identification
     dispatch(loginUser({ email, password, rememberMe }));
   };
-
   // Fonction pour gérer le changement d'état de la case à cocher "Remember Me"
   const handleRememberMeChange = (
     event: React.ChangeEvent<HTMLInputElement>
@@ -48,6 +62,7 @@ export const Login = () => {
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
+          {errorMessage && <div className="error-message">{errorMessage}</div>}
           <div className="input-remember">
             <input
               type="checkbox"
@@ -56,7 +71,13 @@ export const Login = () => {
             />
             <label htmlFor="remember-me">Remember me</label>
           </div>
-          <button type="submit" className="sign-in-button">
+          <button
+            type="submit"
+            className="sign-in-button"
+            onClick={() => {
+              fetchUserProfile();
+            }}
+          >
             Sign In
           </button>
         </form>
